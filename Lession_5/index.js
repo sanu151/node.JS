cecd3323387d388f18e491e8094101154886614d
Supriyo Das <das.supriyo07@gmail.com>
2024-04-20 05:29:42 +0530

    External module - random fruits name

---
+++ Lession_5/index.js
@@ -0,0 +1,7 @@
+// External module - managed by npm
+
+// module : random fruit name
+const getRandomFruitsName = require("random-fruits-name");
+const fruitName = require("random-fruits-name");
+
+console.log(getRandomFruitsName());
